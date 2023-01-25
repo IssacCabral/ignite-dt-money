49f8ff1ef379d29ff71a96e346a9f815dc7f5b9a
Issac Cabral <issaccabrall@gmail.com>
2023-01-25 14:35:30 -0300

    feat: add form structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -7,8 +8,21 @@ interface NewTransactionModalProps {
 
 const NewTransactionModal = (props: NewTransactionModalProps) => {
   return (
-    <Modal isOpen={props.isOpen} onRequestClose={props.onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={props.isOpen}
+      onRequestClose={props.onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" min={0.1} />
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 };
